f25172683c4215d1070e427547a54928d483a787
Kevin Armstrong <xsokev@gmail.com>
2012-07-23 14:23:58 -0500

    TODO: add logic to test module events

--- src/bootstrap/tests/complete.js
+++ src/bootstrap/tests/complete.js
@@ -1,2 +1,3 @@
+//TODO: add module events to tests
 define(['./nodedata', './alert', './button', './dropdown', './modal', './popover', './tab', './tooltip', './scrollspy'], {});
 //define(['./scrollspy'], {});
