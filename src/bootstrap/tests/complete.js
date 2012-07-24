f6cd5360c52e4e519744a96d452887aca73fd3be
Kevin Armstrong <xsokev@gmail.com>
2012-07-23 21:54:20 -0500

    Duplicate unit tests from jQuery plugins

--- src/bootstrap/tests/complete.js
+++ src/bootstrap/tests/complete.js
@@ -1,3 +1,4 @@
+//TODO: copy unit tests from the bootstrap folder
 //TODO: add module events to tests
 define(['./nodedata', './alert', './button', './dropdown', './modal', './popover', './tab', './tooltip', './scrollspy'], {});
 //define(['./scrollspy'], {});
