c0a531b3062590bb028cbd34422fca1667107a4a
Kevin Armstrong <xsokev@gmail.com>
2012-07-23 23:01:51 -0500

    Finished coding module. Debug and testing required.

--- src/bootstrap/Collapse.js
+++ src/bootstrap/Collapse.js
@@ -16,10 +16,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * ========================================================== */
-define([ 
-	'dojo/_base/declare', 
-	'dojo/query', 
-	'dojo/_base/lang', 
+define([
+	'dojo/_base/declare',
+	'dojo/query',
+	'dojo/_base/lang',
 	'dojo/_base/window',
 	'dojo/on',
 	'dojo/dom-class',
@@ -29,9 +29,9 @@ define([
 	'./support/string',
 	'dojo/dom-geometry',
 	'dojo/dom-style',
-	'dojo/NodeList-dom', 
+	'dojo/NodeList-dom',
 	'dojo/NodeList-traverse',
-	'dojo/domReady!' 
+	'dojo/domReady!'
 ], function (declare, query, lang, win, on, domClass, domAttr, trans, nodeData, string, domGeom, domStyle){
 	var collapseSelector = '[data-toggle=collapse]';
 	var Collapse = declare([], {
@@ -41,46 +41,68 @@ define([
 		constructor: function(element, options) {
 			this.options = lang.mixin(lang.clone(this.defaultOptions), (options || {}));
 			this.domNode = element;
-		    if(this.options.parent){
-			    this.parent = query(this.options.parent);
-		    }
-		    this.options.toggle && this.toggle();
+            if (this.options.parent) {
+                this.parent = query(this.options.parent);
+            }
+            this.options.toggle && this.toggle();
 		},
-		dimension: function(target) {
-			return domClass.contains(this.element, 'width') ? 'width' : 'height';
+		dimension: function() {
+			return domClass.contains(this.domNode, 'width') ? 'width' : 'height';
 		},
-		show: function(target){
+		show: function(){
 			var dimension, scroll, actives, hasData;
-			
+
 			if(this.transitioning){ return; }
-			
+
 			dimension = this.dimension();
 			scroll = string.toCamel(['scroll', dimension].join('-'));
-console.log(scroll);
 			actives = this.parent && query('> .accordion-group > .in', this.parent);
-			
+
 			if(actives && actives.length){
 				hasData = nodeData.get(actives[0], 'collapse');
 				if(hasData && hasData.transitioning){ return; }
 				actives.collapse('hide');
 				hasData || nodeData.set(actives[0], 'collapse', null);
 			}
-			
+
 			domStyle.set(this.domNode, dimension, 0);
-//			this.transition('addClass', $.Event('show'), 'shown')	//FIX
+			this.transition('add', 'show', 'shown');
 			domStyle.set(this.domNode, dimension, this.domNode[scroll]);
 		},
-		hide: function(target) {
-			console.log("hide");
-		},
-		reset: function(target) {
-			console.log("reset");
-		},
+		hide: function() {
+            if (this.transitioning) { return; }
+            var dimension = this.dimension();
+            this.reset(domStyle.get(this.domNode, dimension));
+            this.transition('remove', 'hide', 'hidden');
+            domStyle.set(this.domNode, dimension, 0);
+        },
+		reset: function(size) {
+            var dimension = this.dimension();
+            domClass.remove(this.domNode, 'collapse');
+            domStyle.set(this.domNode, dimension, (size || 'auto'));
+            this.domNode.offsetWidth;
+            domClass[(size !== null ? 'add' : 'remove')](this.domNode, 'collapse');
+            return this;
+        },
 		transition: function(method, startEvent, completeEvent){
-			console.log("transition");
-		},
-		toggle: function(target) {
-			this[domClass.contains(this.element, 'in') ? 'hide' : 'show']();
+            var _this = this,
+                _complete = function () {
+                    if (startEvent == 'show') { _this.reset(); }
+                    _this.transitioning = 0;
+                    on.emit(_this.domNode, completeEvent, {bubbles: false, cancelable: false});
+                };
+
+            on.emit(this.domNode, startEvent, {bubbles: false, cancelable: false});
+
+            this.transitioning = 1;
+
+            domClass[method](this.domNode, 'in');
+
+            trans && domClass.contains(this.domNode, 'collapse') ?
+                on.once(_this.domNode, trans.end, _complete) : _complete();
+        },
+		toggle: function () {
+			this[domClass.contains(this.domNode, 'in') ? 'hide' : 'show']();
 		}
 	});
 	lang.extend(query.NodeList, {
@@ -97,15 +119,15 @@ console.log(scroll);
 			});
 		}
 	});
-	
+
 	on(win.body(), on.selector(collapseSelector, 'click'), function(e){
 		var node = query(e.target)[0];
-		var href, target = domAttr.get(node, 'data-target') 
-			|| e.preventDefault() 
+		var href, target = domAttr.get(node, 'data-target')
+			|| e.preventDefault()
 			|| (href = domAttr.get(node, 'href')) && href.replace(/.*(?=#[^\s]+$)/, ''); //strip for ie7
 		nodeData.load(target);
 		var option = nodeData.get(target, 'collapse') ? 'toggle' : nodeData.get(target);
 		query(target).collapse(option);
-	})
+	});
 	return Collapse;
 });
