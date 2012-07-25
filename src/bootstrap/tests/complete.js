47f3442096afcac61672956c9033b06139bf61c8
Kevin Armstrong <xsokev@gmail.com>
2012-07-24 20:07:09 -0500

    Moved test scripts

--- src/bootstrap/tests/complete.js
+++
@@ -1,4 +0,0 @@
-//TODO: copy unit tests from the bootstrap folder
-//TODO: add module events to tests
-define(['./nodedata', './alert', './button', './dropdown', './modal', './popover', './tab', './tooltip', './scrollspy'], {});
-//define(['./scrollspy'], {});
