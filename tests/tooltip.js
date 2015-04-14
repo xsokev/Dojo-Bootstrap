16b462cf6a74df8c3dd65d97e22afdf37497ddc5
Scott Davis <stdavis@utah.gov>
2015-04-14 15:25:25 -0600

    Ported tooltip tests to intern
    Added failing test for `container: body` option.
    
    Also upgraded test bootstrap CSS to latest version.

--- tests/tooltip.js
+++ tests/tooltip.js
@@ -1,38 +1,48 @@
-require({},[
-    "doh",
+define([
+    'intern!object',
+    'intern/chai!assert',
     "dojo/on",
     "dojo/dom-construct",
     "dojo/dom-class",
     "dojo/dom-attr",
     "bootstrap/Tooltip"
-], function (doh, on, domConstruct, domClass, domAttr, Tooltip) {
+], function (registerSuite, assert, on, domConstruct, domClass, domAttr, Tooltip) {
     "use strict";
     var q = dojo.query;
     var tooltipHtml = '<a class="btn" rel="tooltip" title="Tooltip Title">Tooltip</a>';
+    var node;
 
-    doh.register("bootstrap.tooltips", {
+    registerSuite({
+        name: "tooltips",
+        beforeEach: function () {
+            node = domConstruct.place(tooltipHtml, document.body);
+        },
         "should be defined on NodeList object":function () {
-            doh.t(q(document.body).tooltip);
+            assert.ok(q(document.body).tooltip);
         },
         "should return element":function () {
-            doh.is(document.body, q(document.body).tooltip()[0]);
+            assert.equal(document.body, q(document.body).tooltip()[0]);
         },
-        "should remove title attribute": {
-            setUp:function () { this.t = domConstruct.place(tooltipHtml, document.body); },
-            runTest:function () {
-                q(this.t).tooltip();
-                doh.f(domAttr.get(this.t, 'title'));
-            },
-            tearDown:function () { domConstruct.destroy(this.t); }
+        "should remove title attribute": function () {
+            q(node).tooltip();
+            assert.notOk(domAttr.get(node, 'title'));
         },
-        "should add data attribute for referencing original title": {
-            setUp:function () { this.t = domConstruct.place(tooltipHtml, document.body); },
-            runTest:function () {
-                var title = domAttr.get(this.t, 'title');
-                q(this.t).tooltip();
-                doh.is(title, domAttr.get(this.t, 'data-original-title'));
-            },
-            tearDown:function () { domConstruct.destroy(this.t); }
+        "should add data attribute for referencing original title": function () {
+            var title = domAttr.get(node, 'title');
+            q(node).tooltip();
+            assert.equal(title, domAttr.get(node, 'data-original-title'));
+        },
+        "should append tooltip to body when container is defined as such": function () {
+            q(node).tooltip({
+                container: 'body'
+            });
+            q(node).tooltip('show');
+            assert.equal(q('body>.tooltip').length, 1);
+        },
+        afterEach: function () {
+            if (node) {
+                domConstruct.destroy(node);
+            }
         }
     });
 });
